7ccdec4cd41161c12ea95c5c6cf100c5ffc0aba3
Mohamed Nifras <84182306+NIFARS@users.noreply.github.com>
2026-01-07 10:45:10 +0530

    Use environment variable for uvicorn port

--- main.py
+++ main.py
@@ -542,5 +542,7 @@ async def startup():
 
 # ==================== Local Testing ====================
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
